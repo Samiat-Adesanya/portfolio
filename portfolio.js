524b68a864581c836b81dcce62ebe0431cb5d984
Samiat-Adesanya <145464075+Samiat-Adesanya@users.noreply.github.com>
2024-01-17 14:14:47 -0800

    portfolio project added

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+
+setTimeout(() => {
+    const animation = document.querySelector(".front");
+  animation.style.display = "none";
+  const portfolio = document.querySelector(".every");
+  portfolio.style.display = "block";
+}, 4000); 
